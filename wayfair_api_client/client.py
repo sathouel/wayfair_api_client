18df9b12488f717ea4abc4b1f4b1320ae84c28f8
steven athouel <sathouel@gmail.com>
2020-05-19 16:10:20 +0300

    gql client added

--- wayfair_api_client/client.py
+++ wayfair_api_client/client.py
@@ -1,6 +1,9 @@
 import json
 
 import requests as rq
+from gql import gql, Client as GQLClient
+from gql.transport.requests import RequestsHTTPTransport
+import six
 
 from . import (
     utils
@@ -18,14 +21,27 @@ class WayfairAPICLient:
         self._client_secret = client_secret
 
         self._authtenticate()
+        self._init_gql_client()
 
     @property
     def endpoints(self):
+        api_base_url = self.SANDBOX_BASE_URL if self._sandbox else self.BASE_URL
         endpoints = {
-            'api': self.SANDBOX_BASE_URL if self._sandbox else self.BASE_URL,
-            'gql': utils.urljoin(self.BASE_URL, 'v1/graphql'),
+            'api': api_base_url,
+            'gql': utils.urljoin(api_base_url, 'v1/graphql'),
         }
         return endpoints
+
+    def execute(self, query):
+        if isinstance(query, six.string_types):
+            query = gql(query)
+        return self._gql_client.execute(query)
+
+    def _init_gql_client(self):
+        session_auth_header_value = self._session.headers.get('Authorization')
+        self._gql_client = GQLClient(transport=RequestsHTTPTransport(
+            url=self.endpoints.get('gql'),
+            headers={'Authorization': session_auth_header_value}))
 
     def _authtenticate(self):
         payload = {
@@ -42,9 +58,9 @@ class WayfairAPICLient:
         if res.status_code != 200:
             raise ValueError('Authentication Failed, Please check your credentials')
 
-        token_type, access_token = res.json().get('token_type'), res.json().get('access_token')
+        self._token_type, self._access_token = res.json().get('token_type'), res.json().get('access_token')
         auth_headers = {
-            'Authorization': '{} {}'.format(token_type, access_token),
+            'Authorization': '{} {}'.format(self._token_type, self._access_token),
             'Content-Type': 'application/json',
         }
         self._session.headers.update(auth_headers)
