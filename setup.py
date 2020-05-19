f08d45d542b2ca7fa94b283dd4d02121aa910c7b
steven athouel <sathouel@gmail.com>
2020-05-19 15:46:14 +0300

    setup added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wayfair-api-client",
+    version="0.0.1",
+    author="Steven Athouel",
+    author_email="sathouel@gmail.com",
+    description="A simple api client for bobochic suppliers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sathouel/wayfai_api_client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ wayfair_api_client/client.py
@@ -0,0 +1,51 @@
+import json
+
+import requests as rq
+
+from . import (
+    utils
+)
+
+class WayfairAPICLient:
+    BASE_URL = 'https://api.wayfair.com'
+    SANDBOX_BASE_URL = 'https://sandbox.api.wayfair.com'
+    AUTH_URL = 'https://sso.auth.wayfair.com/oauth/token'
+
+    def __init__(self, client_id, client_secret, sandbox=False):
+        self._session = rq.Session()
+        self._sandbox = sandbox
+        self._client_id = client_id
+        self._client_secret = client_secret
+
+        self._authtenticate()
+
+    @property
+    def endpoints(self):
+        endpoints = {
+            'api': self.SANDBOX_BASE_URL if self._sandbox else self.BASE_URL,
+            'gql': utils.urljoin(self.BASE_URL, 'v1/graphql'),
+        }
+        return endpoints
+
+    def _authtenticate(self):
+        payload = {
+            "grant_type":"client_credentials",
+            "client_id": self._client_id,
+            "client_secret": self._client_secret,
+            "audience": self.BASE_URL
+        }
+        headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+        }
+        res = self._session.post(self.AUTH_URL, data=json.dumps(payload), headers=headers)
+        if res.status_code != 200:
+            raise ValueError('Authentication Failed, Please check your credentials')
+
+        token_type, access_token = res.json().get('token_type'), res.json().get('access_token')
+        auth_headers = {
+            'Authorization': '{} {}'.format(token_type, access_token),
+            'Content-Type': 'application/json',
+        }
+        self._session.headers.update(auth_headers)
+        return res.json()
