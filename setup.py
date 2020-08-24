0293d21fba40caa6389a10e120138c22f62816a6
steven athouel <sathouel@gmail.com>
2020-08-24 14:50:55 +0300

    trivial test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wayfair-api-client",
-    version="0.0.7",
+    version="0.0.8",
     author="Steven Athouel",
     author_email="sathouel@gmail.com",
     description="A simple api client for Wayfair suppliers",
